a5921cba3f37f521372cb64e6124e33fbf531c33
Eric McCormick <edm00se@gmail.com>
2017-11-29 16:46:46 -0600

    updates to use aframe from npm
    js no longer has separate version, bs-config updated for cwd

--- bs-config.js
+++ bs-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  server: "app",
-  files: ["app/*.html", "app/css/*.css", "app/img/*.png", "app/img/*.jpg"]
+  server: ".",
+  files: ["*.html", "css/*.css", "img/*.png", "img/*.jpg"]
 }
